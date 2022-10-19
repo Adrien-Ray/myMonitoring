cbb8d7169953f9517ac09175fdeb04bc8bf8a15f
Adrien-Ray <a.raymond@codeur.online>
2022-10-19 09:48:47 +0200

    begin display btn category

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,6 @@
 import { vue } from "./modules/varStructure.js";
 import { jsonFetch, jsonFetch2 } from "./modules/jsonFetch.js";
+import { createBtnCategory } from "./modules/createBtnCategory.js";
 
 document.getElementById(vue.launch).style.display = 'block';
 document.getElementById('vue_launch_button_all').addEventListener('click', () => {
@@ -23,3 +24,4 @@ document.getElementById('form_url_outpout').addEventListener('click', () => {
     console.log('outpout: ', document.getElementById('form_url_input').value);
     jsonFetch('', document.getElementById('form_url_input').value);
 });
+createBtnCategory();
---
+++ assets/scripts/modules/createBtnCategory.js
@@ -0,0 +1,7 @@
+function createBtnCategory() {
+    console.log('function createBtnCategory');
+    document.getElementById('vue_launch_category').innerHTML = 'function createBtnCategory';
+    document.getElementById('vue_launch_category').style.display = 'block';
+}
+
+export { createBtnCategory };
