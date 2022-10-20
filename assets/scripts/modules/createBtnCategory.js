be18d578a728cccfe5dc2586a7348c110409ea32
Adrien-Ray <a.raymond@codeur.online>
2022-10-20 10:45:41 +0200

    create btn cat

--- assets/scripts/modules/createBtnCategory.js
+++ assets/scripts/modules/createBtnCategory.js
@@ -1,7 +1,49 @@
 function createBtnCategory() {
     console.log('function createBtnCategory');
-    document.getElementById('vue_launch_category').innerHTML = 'function createBtnCategory';
+    document.getElementById('vue_launch_category').innerHTML = 'search category';
     document.getElementById('vue_launch_category').style.display = 'block';
+    var url = './assets/scripts/modules/myData.json';
+    fetch(url)
+        .then(response => response.json())
+        .then(json => {
+            let jsonFilter = json;
+            const arrayOfCat = [];
+            for (let index = 0; index < jsonFilter.category.length; index++) {
+                const element = jsonFilter.category[index];
+                arrayOfCat.push({
+                    category: element,
+                    domain: []
+                });
+                for (let index2 = 0; index2 < jsonFilter.domain.length; index2++) {
+                    const element2 = jsonFilter.domain[index2];
+                    for (let index3 = 0; index3 < element2.category.length; index3++) {
+                        const element3 = element2.category[index3];
+                        if (element3 === element) {
+                            arrayOfCat[index].domain.push(element2);
+                        }
+                    }
+                }
+            }
+            console.log("arrayOfCat", arrayOfCat);
+            const arrayOfCat2 = [];
+            for (let index = 0; index < arrayOfCat.length; index++) {
+                const element = arrayOfCat[index];
+                if (element.domain.length > 0) {
+                    arrayOfCat2.push(element);
+                }
+            }
+            console.log('arrayOfCat2', arrayOfCat2);
+            if (arrayOfCat2.length > 0) {
+                let domForVueLaunchCategory = '';
+                for (let index = 0; index < arrayOfCat2.length; index++) {
+                    const element = arrayOfCat2[index].category;
+                    domForVueLaunchCategory = `${domForVueLaunchCategory}<button class="btn_cat">${element}</button>`;
+                }
+                document.getElementById('vue_launch_category').innerHTML = domForVueLaunchCategory;
+            } else {
+                document.getElementById('vue_launch_category').innerHTML = 'no category found';
+            }
+        });
 }
 
 export { createBtnCategory };
